3140d5fe473f84420cc029080721bcc5dd611aee
PayLink Protocol <mail@paylinkprotocol.com>
2024-11-30 15:28:58 +0100

    Update listen_events.py

--- examples/listen_events.py
+++ examples/listen_events.py
@@ -1,7 +1,7 @@
 from paylink_protocol import PayLinkProtocolManager
 
-RPC_URL = "https://eth-sepolia.g.alchemy.com/v2/lAY70T35Lt5A4ViwfULpWvgeKfA4k5Q5"
-APP_ID = 1732888855
+RPC_URL = ""
+APP_ID = 
 
 manager = PayLinkProtocolManager(RPC_URL, APP_ID)
 manager.subscribe()
